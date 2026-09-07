145448a59109c05331dfa832c9fb2c5b6392dd53
unknown <kshaurya.dev@gmail.com>
2026-09-07 17:48:16 +0530

    some change

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
+const dns = require('dns');
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
